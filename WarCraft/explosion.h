f2588fd668a71558cba4f291f1513930e36998e9
Chu Huy Quang <quangdtm2004@gmail.com>
2023-03-03 01:34:55 +0700

    debug -> realease

---
+++ WarCraft/explosion.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "object.h"
+
+class explosion : public object
+{
+private:
+
+public:
+
+};
--- WarCraft/object.cpp
+++ WarCraft/object.cpp
@@ -1,4 +1,4 @@
-#include <object.h>
+#include "object.h"
 
 object::object(){
     rect.x;
